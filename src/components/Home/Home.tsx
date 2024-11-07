1f034e9f04970b12d47a56ec2ffa69784219c4f1
rostyslav-horytskyi <gorzer.rostyslav@gmail.com>
2024-11-07 19:33:19 +0100

    refactor: improvements

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,13 @@
 import Header from '../Header/Header';
 import { LeftSidebar } from './components/LeftSidebar/LeftSidebar';
 import { RightSidebar } from './components/RightSidebar/RightSidebar';
+import { block } from '../../helpers/bem.helpers';
+import './Home.scss';
+
+const b = block('Home');
 
 export const Home = () => (
-  <div>
+  <div className={b()}>
     <Header />
     {/* TODO: Use add user */}
     <LeftSidebar user={{}} />
